b3e3af6c42a11457e68b447dd580c079555f46dc
Pjijin <Pjijin1@gmail.com>
2019-09-27 21:02:38 +0530

    UI Update

--- src/components/date.component.jsx
+++ src/components/date.component.jsx
@@ -1,97 +1,107 @@
 import React, { useState } from 'react';
-import {  Trash, MinusSquare, Plus } from 'react-feather';
-import uuid from 'uuid'
+import { Trash, MinusSquare, Plus } from 'react-feather';
+import uuid from 'uuid';
 import { Container, Draggable } from 'react-smooth-dnd';
 import List from './list.component';
 
 export default function DateComponent({ taskData, setTasksdata, tasks, index }) {
-    const [dateTasks, setDateTasks ] = useState( taskData.lists);
+	const [dateTasks, setDateTasks] = useState(taskData.lists);
 
-    const updateTaskData = (newFilteredTask) => {
-        tasks[index].lists = newFilteredTask;
-        setTasksdata({
-             data: tasks,
-             lastModified: new Date().getTime()
-        });
-        setDateTasks(newFilteredTask);
-    }
+	const updateTaskData = newFilteredTask => {
+		tasks[index].lists = newFilteredTask;
+		setTasksdata({
+			data: tasks,
+			lastModified: new Date().getTime()
+		});
+		setDateTasks(newFilteredTask);
+	};
 
-    const onDrop = ({ addedIndex, removedIndex }) => {
-        [dateTasks[addedIndex], dateTasks[removedIndex]] = [dateTasks[removedIndex], dateTasks[addedIndex]];
-        updateTaskData(dateTasks);
-    };
+	const onDrop = ({ addedIndex, removedIndex }) => {
+		[dateTasks[addedIndex], dateTasks[removedIndex]] = [dateTasks[removedIndex], dateTasks[addedIndex]];
+		updateTaskData(dateTasks);
+	};
 
-    const removeListItem = (id) => {
-        const newFilteredTask = dateTasks.filter(task => task.id !== id)
-        updateTaskData(newFilteredTask);
-    }
+	const removeListItem = id => {
+		const newFilteredTask = dateTasks.filter(task => task.id !== id);
+		updateTaskData(newFilteredTask);
+	};
 
-    const editTodo = (id,message) => {
-        dateTasks.forEach(tsk => {
-            if(tsk.id === id){
-                tsk.message = message;
-            }
-         });
-        updateTaskData(dateTasks);
-    }
+	const editTodo = (id, message) => {
+		dateTasks.forEach(tsk => {
+			if (tsk.id === id) {
+				tsk.message = message;
+			}
+		});
+		updateTaskData(dateTasks);
+	};
 
-    const clearListData = () =>  updateTaskData([]);
+	const clearListData = () => updateTaskData([]);
 
+	const addNewList = () => {
+		const newFilteredTask = [
+			...dateTasks,
+			{
+				id: uuid(),
+				message: 'new',
+				completed: false
+			}
+		];
+		updateTaskData(newFilteredTask);
+	};
 
-    const addNewList = () => {
-        const newFilteredTask = [...dateTasks, {
-                    id: uuid(),
-					message: 'new',
-					completed:false
-				}]
-       updateTaskData(newFilteredTask);
-    }
+	const deleteDate = () =>
+		setTasksdata({
+			data: tasks.filter(el => el.date !== taskData.date),
+			lastModified: new Date().getTime()
+		});
 
-    const deleteDate = () => setTasksdata({
-             data: tasks.filter(el => el.date !== taskData.date),
-             lastModified: new Date().getTime()
-    });
+	const markAsComplete = id => {
+		dateTasks.forEach(task => {
+			if (task.id === id) {
+				task.completed = !task.completed;
+			}
+		});
 
+		updateTaskData(dateTasks);
+	};
 
-    const markAsComplete = (id) => {
-        dateTasks.forEach(task => {
-            if(task.id === id){
-                task.completed = !task.completed;
-            }
-        });
+	return (
+		<div className="date-todo">
+			<div className="date-style">
+				<span>{taskData.date}</span>
+				<div className="actions">
+					<button>
+						<Trash size="14" data-tip="Remove Day" onClick={deleteDate} />
+					</button>
+					<button>
+						<MinusSquare data-tip="Clear List" onClick={clearListData} size="14" />
+					</button>
+				</div>
+			</div>
+			<ul>
+				<Container onDrop={onDrop}>
+					{dateTasks.map(task => {
+						return (
+							<Draggable key={task.id}>
+								<List
+									{...task}
+									key={task}
+									removeListItem={removeListItem}
+									editTodo={editTodo}
+									markAsComplete={markAsComplete}
+								/>
+							</Draggable>
+						);
+					})}
 
-        updateTaskData(dateTasks);
-    }
-
-	return <div className="date-todo">
-        <div className="date-style">
-            <span>{ taskData.date}</span>
-            <div className="actions">
-                <button>
-                    <Trash size="14"  data-tip="Remove Day"  onClick={ deleteDate} />
-                </button>
-                <button>
-                    <MinusSquare data-tip="Clear List" onClick={clearListData} size="14" />
-                </button>
-            </div>
-        </div>
-    	<ul>
-					<Container onDrop={onDrop}>
-						{dateTasks.map(task => {
-							return (
-								<Draggable key={task.id}>
-									<List {...task} key={task} removeListItem={removeListItem} editTodo={editTodo} markAsComplete={markAsComplete}/>
-								</Draggable>
-							);
-                        })}
-
-                        {dateTasks.length === 0 && <li className="empty-list">You have not added any items</li> }
-                        <li className="new-list">
-                          <button>
-                              <Plus data-tip="New Task"  onClick={addNewList} size="12" />
-                          </button>
-                        </li>
-					</Container>
-		</ul>
-    </div>;
+					{dateTasks.length === 0 && <li className="empty-list">You have not added any items</li>}
+					<li className="new-list">
+						<button>
+							<Plus data-tip="New Task" onClick={addNewList} size="12" />
+						</button>
+					</li>
+				</Container>
+			</ul>
+		</div>
+	);
 }
--- src/components/plan.component.jsx
+++ src/components/plan.component.jsx
@@ -1,26 +1,32 @@
 import React from 'react';
-import moment from "moment";
+import moment from 'moment';
 
-export default function Plan({ tasks , setTasksdata}) {
-	const planAnotherDay = (e) => {
+export default function Plan({ tasks, setTasksdata }) {
+	const lastDate = tasks.length !== 0 && tasks[tasks.length - 1].date;
+	console.log(lastDate);
+	const planAnotherDay = e => {
 		const newDateObject = [];
 
 		[...Array(Number(e.target.value)).keys()].map(dt => {
-			const nDate = (moment().add(dt+1, 'days').format('dddd, DD MMMM YYYY'));
-			newDateObject.push({date: nDate, lists:[]})
+			const nDate = moment()
+				.add(dt + 1, 'days')
+				.format('dddd, DD MMMM YYYY');
+			newDateObject.push({ date: nDate, lists: [] });
 			return 1;
-		})
+		});
 
 		const newData = {
 			data: [...tasks, ...newDateObject],
 			lastModified: new Date().getTime()
-		}
+		};
 
 		setTasksdata(newData);
-	}
+	};
 	return (
 		<div className="planner">
-			Plan next <input onChange={planAnotherDay} type="number" className="plan-days" placeholder="1" /> day(s)
+			Plan next
+			<input onChange={planAnotherDay} type="number" className="plan-days" placeholder="1" min="0" max="10" />
+			day(s)
 		</div>
 	);
 }
